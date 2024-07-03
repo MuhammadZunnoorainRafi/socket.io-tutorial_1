6e72c0d70af3cf1486d92c303c2c161d1b035f89
Zunnoorain Rafi <zunnoorainrafi236@gmail.com>
2024-07-04 01:52:30 +0500

    tutorial 1 completed

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
 
 const PORT = 5000;
 const app = express();
@@ -21,9 +23,22 @@ const io = new Server(server, {
   },
 });
 
+app.get('/login', (req, res) => {
+  const token = jwt.sign({ id: 'asjdflajdf' }, 'asdfadsf', { expiresIn: '1d' });
+  res
+    .cookie('token', token, { httpOnly: true, secure: true })
+    .json({ message: 'User Login successfully ' });
+});
+
 io.on('connection', (socket) => {
-  console.log('User Connected');
-  console.log('ID: ', socket.id);
+  socket.emit('welcome', 'Welcome from the server');
+  console.log('USER CONNECTED');
+  socket.on('message', ({ room, message }) => {
+    socket.to(room).emit('response', `Message Response = ${message}`);
+  });
+  socket.on('join-room', (room) => {
+    socket.join(room);
+  });
 });
 
 app.get('/', (req, res) => {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,114 @@
+import { FormEvent, useEffect, useMemo, useState } from 'react';
 import { io } from 'socket.io-client';
 
 function App() {
-  const socket = io('http://localhost:5000');
+  const [responseMessages, setResponseMessages] = useState<string[]>([]);
+  const [message, setMessage] = useState('');
+  const [socketID, setSocketID] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [room, setRoom] = useState('');
+  const [joinRoom, setJoinRoom] = useState('');
 
-  return <div>Hello Vite!</div>;
+  const socket = useMemo(
+    () => io('http://localhost:5000', { withCredentials: true }),
+    []
+  );
+
+  useEffect(() => {
+    socket.on('welcome', () => {
+      setSocketID(socket.id!);
+    });
+    socket.on('response', (val) => {
+      setResponseMessages((prev) => [...prev, val]);
+    });
+  }, [socket]);
+
+  const formSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (message.length === 0) {
+      setErrorMessage('Enter Text');
+    } else {
+      socket.emit('message', { room, message });
+      setMessage('');
+      setErrorMessage('');
+    }
+  };
+
+  const formSubmitRoom = (e: FormEvent) => {
+    e.preventDefault();
+    socket.emit('join-room', joinRoom);
+  };
+
+  return (
+    <div className="container mx-auto ">
+      <h1 className="text-center font-bold text-5xl my-6">
+        Socket.io Tutorial
+      </h1>
+      <h4 className="text-center font-semibold mb-2">{socketID}</h4>
+      <form onSubmit={formSubmitRoom}>
+        <input
+          type="text"
+          value={joinRoom}
+          onChange={(e) => {
+            setJoinRoom(e.target.value);
+          }}
+          placeholder="Enter text"
+          className="p-1 rounded-md w-full placeholder:text-slate-400"
+        />
+        <button
+          type="submit"
+          className="bg-indigo-600 px-3 py-1 text-center font-semibold text-sm inline-block text-white cursor-pointer uppercase transition duration-200 ease-in-out rounded-md hover:bg-indigo-700  active:scale-95"
+        >
+          Join
+        </button>
+      </form>
+      <form
+        onSubmit={formSubmit}
+        className="border border-slate-300 max-w-md mx-auto rounded-xl p-5 bg-slate-100 flex items-center gap-2"
+      >
+        <div>
+          <input
+            type="text"
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+              setErrorMessage('');
+            }}
+            placeholder="Enter text"
+            className="p-1 rounded-md w-full placeholder:text-slate-400"
+          />
+          {errorMessage.length > 0 && (
+            <p className="text-red text-sm text-red-500">{errorMessage}</p>
+          )}
+        </div>
+        <input
+          type="text"
+          value={room}
+          onChange={(e) => {
+            setRoom(e.target.value);
+          }}
+          placeholder="Enter Room"
+          className="p-1 rounded-md w-full placeholder:text-slate-400"
+        />
+        <button
+          type="submit"
+          className="bg-indigo-600 px-3 py-1 text-center font-semibold text-sm inline-block text-white cursor-pointer uppercase transition duration-200 ease-in-out rounded-md hover:bg-indigo-700  active:scale-95"
+        >
+          Send
+        </button>
+      </form>
+      <div>
+        <h2 className="font-semibold tracking-widest text-xl underline text-slate-900 py-2">
+          Messages:
+        </h2>
+        {responseMessages.map((val, ind) => (
+          <p key={ind} className="text-slate-700 ">
+            {val}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
